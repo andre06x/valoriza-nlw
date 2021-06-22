2dd32c0e477ffd7fef5d724f7f455d4cf8d8c33b
unknown <andre06x@gmail.com>
2021-06-22 02:22:12 -0300

    dia2

---
+++ src/database/migrations/1624333149715-CrateUsers.ts
@@ -0,0 +1,47 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CrateUsers1624333149715 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "user",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "name",
+                        type: "varchar"
+                    },
+                    {
+                        name: "email",
+                        type: "varchar",
+                    },
+                    {
+                        name: "admin",
+                        type: "boolean",
+                        default: false
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "update",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
+    }
+
+}
---
+++ src/services/createUserService.ts
@@ -0,0 +1,37 @@
+import { getCustomRepository } from 'typeorm';
+import { UserRepositories } from '../database/repositories/UsersRepositories';
+
+interface UserRequest {
+    name: string;
+    email: string;
+    admin?: boolean
+}
+
+class CreateUserService {
+    async execute({name, email, admin} : UserRequest){
+        const usersRepository = getCustomRepository(UserRepositories);
+
+        if(!email){
+            throw new Error("Email incorrect");
+        }
+        const userAlreadyExists = await usersRepository.findOne({
+            email
+        });
+
+        if(userAlreadyExists){
+            throw new Error("User already exists");
+        }
+
+        const user = usersRepository.create({
+            name,
+            email,
+            admin,
+        })
+
+        await usersRepository.save(user);
+
+        return user;
+    }
+}
+
+export { CreateUserService }
